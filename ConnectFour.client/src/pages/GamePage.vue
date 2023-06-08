d8c8a342f6621827020d2f92b1a9f873e596bda3
Ryan-Thrall <ryanthrall04@gmail.com>
2023-06-08 15:23:40 -0600

    Setup Canvas

--- ConnectFour.client/src/pages/GamePage.vue
+++ ConnectFour.client/src/pages/GamePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="component">
-
+    <canvas></canvas>
 
   </div>
 </template>
@@ -15,4 +15,18 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+canvas {
+  background-color: lightgray;
+    padding: 0;
+    margin: auto;
+    display: block;
+    width: 1280px;
+    height: 720px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+</style>
